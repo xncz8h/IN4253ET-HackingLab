a2610ffb232aec56a3080ba696aecd5bbc5de03a
Boris van Groeningen <b.vangroeningen@student.tudelft.nl>
2022-03-04 13:08:25 +0100

    Add loop for all websites

--- cookie.py
+++ cookie.py
@@ -1,21 +1,16 @@
-from dataclasses import dataclass
 
 
-@dataclass
-class DataCookie:
-    name: str
-    src_domain: str
-    domain: str
-    ttl: float
-    trackers: []
-    is_third_party: bool = True
-
-
-class Cookie(object):
-    def __init__(self, name, src_domain, domain, ttl, isThirdParty=False):
+class Cookie:
+    def __init__(self, name, src_domain, domain, ttl, isThirdParty):
         self.name = name
         self.src_domain = src_domain
         self.domain = domain
         self.ttl = ttl
         self.isThirdParty = isThirdParty
         self.trackers = []
+
+    def __repr__(self):
+        return f'Cookie(name={self.name}, src_domain={self.src_domain}, domain={self.domain}, ttl={self.ttl}, ' \
+               f'isThirdParty={self.isThirdParty}, tracker={self.trackers})\n'
+
+
